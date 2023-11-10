47ee395724130f0d0bc4e4ffdbbeb0b4c4544132
Your Name <you@example.com>
2023-11-10 15:05:18 +0100

    up

--- src/utils/router.jsx
+++ src/utils/router.jsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Root from '../components/Root';
 import Location from '../pages/Location';
-<<<<<<< HEAD
+
 import { NotFoundPage } from '../pages/NotFoundPage';
-=======
+
 import Dashboard from '../pages/Dashboard';
->>>>>>> e47587f1cce4c358f4c59c2fc352c2dbd2eb6675
+
 
 
 export const router = createBrowserRouter([
